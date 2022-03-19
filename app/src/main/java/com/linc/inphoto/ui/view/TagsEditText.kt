e514ccff145c00ee919a6846d5a5cfea9c07bad4
Andrew <andrew.linc.work@gmail.com>
2022-03-19 20:31:29 +0200

    Added input filter in TagsEditText [INPH-4]

--- app/src/main/java/com/linc/inphoto/ui/view/TagsEditText.kt
+++ app/src/main/java/com/linc/inphoto/ui/view/TagsEditText.kt
@@ -1,6 +1,7 @@
 package com.linc.inphoto.ui.view
 
 import android.content.Context
+import android.text.InputType
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.widget.doOnTextChanged
@@ -8,14 +9,21 @@ import androidx.transition.AutoTransition
 import androidx.transition.TransitionManager
 import com.linc.inphoto.R
 import com.linc.inphoto.databinding.LayoutTagsEditTextBinding
+import com.linc.inphoto.utils.TextInputFilter
+import com.linc.inphoto.utils.extensions.EMPTY
 import com.linc.inphoto.utils.extensions.inflater
 import com.linc.inphoto.utils.extensions.view.addChip
+import com.linc.inphoto.utils.extensions.view.textToString
 
 
 class TagsEditText constructor(
     context: Context,
     attrs: AttributeSet
 ) : ConstraintLayout(context, attrs) {
+
+    companion object {
+        private const val TAG_LENGTH = 12
+    }
 
     private var binding: LayoutTagsEditTextBinding? = null
     private val tags = mutableListOf<String>()
@@ -30,20 +38,48 @@ class TagsEditText constructor(
 
         binding = LayoutTagsEditTextBinding.inflate(context.inflater, this, true)
         binding?.run {
-            tagsEditText.doOnTextChanged { tag, _, _, _ ->
-                if (tag.isNullOrEmpty() || tag.isBlank()) {
-                    tagsEditText.text?.clear()
-                    return@doOnTextChanged
-                }
+            tagsEditText.apply {
+                inputType =
+                    InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                doOnTextChanged { tag, _, _, _ ->
+                    if (tag.isNullOrEmpty() || tag.isBlank()) {
+                        tagsEditText.text?.clear()
+                        return@doOnTextChanged
+                    }
 
-                if (tag.last().isWhitespace()) {
-                    val tagAdded = addChip(tag.toString())
+                    if (tag.last().isWhitespace()) {
+                        val tagAdded = addChip(tag.toString())
+                        if (tagAdded) {
+                            tagsEditText.text?.clear()
+                        } else {
+                            tagsEditText.text?.delete(tag.length - 1, tag.length)
+                        }
+                    }
+                }
+                setOnFocusChangeListener { _, hasFocus ->
+                    if (hasFocus) {
+                        return@setOnFocusChangeListener
+                    }
+                    val tag = textToString()
+                    if (tag.isEmpty() || tag.isBlank()) {
+                        tagsEditText.text?.clear()
+                        return@setOnFocusChangeListener
+                    }
+
+                    val tagAdded = addChip(tag)
                     if (tagAdded) {
                         tagsEditText.text?.clear()
                     } else {
                         tagsEditText.text?.delete(tag.length - 1, tag.length)
                     }
                 }
+                filters = arrayOf(TextInputFilter { text, start, end, spanned, _, _ ->
+                    if (spanned?.length ?: 0 >= TAG_LENGTH - 1 && text?.isBlank() == false) {
+                        return@TextInputFilter String.EMPTY
+                    }
+                    return@TextInputFilter text?.subSequence(start, end)
+                        ?.replace(Regex("([^A-Za-z0-9\\p{L}_ ])"), String.EMPTY)
+                })
             }
         }
     }
--- app/src/main/java/com/linc/inphoto/utils/extensions/StringExt.kt
+++ app/src/main/java/com/linc/inphoto/utils/extensions/StringExt.kt
@@ -1,5 +1,9 @@
 package com.linc.inphoto.utils.extensions
 
+val String.Companion.EMPTY get() = ""
+
 fun String?.toException() = Exception(this)
 
-val String.Companion.EMPTY get() = ""
+fun String.removeSpaces() = filter { !it.isWhitespace() }
+
+fun Char.isOneOf(vararg symbols: Char) = symbols.any { this == it }
